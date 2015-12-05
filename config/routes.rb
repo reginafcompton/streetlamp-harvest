179bce457d27338230b0348f41bf6786f9db3c55
Regina Compton <reginafcompton@gmail.com>
2015-12-05 15:07:20 -0600

    Update layout. Add comment model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  root 'posts#index'
+  get 'about/index'
 
-  resources :posts
+  get 'welcome/index'
+
+  root 'welcome#index'
+
+  resources :posts, :comments
   devise_for :users, controllers: { registrations: 'registrations' }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+posts = Post.create([{ title: 'University of Chicago, South Quad', content: '<p>This handsome little fellow lives in the South Quad at the University of Chicago. Alone among simple mums, it does not scream of wastefulness, but rather sits demurely and dignified, putting on airs as listless university humanists walk by.</p><p>As far as edibility goes, I give this cabbage a 2.5 out of 5 leaves. It has a leathery texture, and though reminiscent of kale, it is wanting in flavor. I do not recommend this particular vegetable for eating, even to the hungriest UofC graduate student.</p>'}])
+
+
+comments = Comments.create([{content: "Very interesting", user_id: 1, post_id: 1}])
