e524ea80d2285f83d527cd01e288c6fd82e0f2ed
reginafcompton <reginafcompton@gmail.com>
2016-01-28 10:20:13 -0600

    Delete coffee files

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -4,16 +4,16 @@ $(document).ready(function(){
     $(".comment-form").toggle();
   });
 
-  $("#nav-mobile").html($("#nav-main").html());
+  // $("#nav-mobile").html($("#nav-main").html());
 
-  $("#nav-trigger span").on("click", function() {
-    console.log("THIS");
-    if ($("nav#nav-mobile ul").hasClass("expanded")) {
-      $("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp();
-      $(this).removeClass("open"); }
-    else {
-      $("nav#nav-mobile ul.expanded").addClass("expanded").slideDown();
-      $(this).addClass("open");
-    }
-  });
+  // $("#nav-trigger span").on("click", function() {
+  //   console.log("THIS");
+  //   if ($("nav#nav-mobile ul").hasClass("expanded")) {
+  //     $("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp();
+  //     $(this).removeClass("open"); }
+  //   else {
+  //     $("nav#nav-mobile ul.expanded").addClass("expanded").slideDown();
+  //     $(this).addClass("open");
+  //   }
+  // });
 });
