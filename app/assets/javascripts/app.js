948ad3809e94a1717be911851d8d7374b501e1e6
reginafcompton <reginafcompton@gmail.com>
2016-01-29 21:17:25 -0600

    Create popup window for new post

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,4 +3,22 @@ $(document).ready(function(){
     event.preventDefault();
     $(".comment-form").toggle();
   });
+
+  openPopUp();
+  closePopUp();
 });
+
+function openPopUp() {
+  $("#new-post").on("click", function() {
+    var popupBox = $(".popup-new");
+    popupBox.fadeIn(600);
+    $("#mask").fadeIn(600);
+  });
+}
+
+function closePopUp() {
+  $("body").on("click", ".close", function() {
+    $(".popup-new").fadeOut(600);
+    $("#mask").fadeOut(600);
+  });
+}
