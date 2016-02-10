1c7ab2fad5629119ce6a31d776b18bd13a42b29c
reginafcompton <reginafcompton@gmail.com>
2016-02-10 10:00:46 -0600

    Add controllers for user, and make plans to add views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  devise_for :users
   root 'welcome#index'
   get 'welcome/index'
   get 'about/index'
@@ -12,6 +11,10 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
+
+  devise_for :users, :controllers => { registrations: 'registrations' }
+
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
