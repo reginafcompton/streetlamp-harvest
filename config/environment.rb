71bc6ae19f15a96494f9e82efeb290bfe4f32717
reginafcompton <reginafcompton@gmail.com>
2016-02-11 15:57:43 -0600

    Add Actionmailer to environment

--- config/environment.rb
+++ config/environment.rb
@@ -7,3 +7,14 @@ Rails.application.initialize!
 # Load the app's custom environment variables here, before environments/*.rb
 app_env_vars = File.join(Rails.root, 'config', 'initializers', 'app_env_vars.rb')
 load(app_env_vars) if File.exists?(app_env_vars)
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
