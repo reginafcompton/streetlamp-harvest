64612381c591e0467ea8574ca104f31556004efb
Regina Compton <reginafcompton@gmail.com>
2016-04-06 12:13:18 -0500

    Write thrid routing spec

--- spec/routing/site_routing_spec.rb
+++ spec/routing/site_routing_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe WelcomeController, :type => :routing do 
-	describe "routing" do 
+	describe "routing to homepage" do 
 		it "routes to root" do
 			expect(:get => root_path).to route_to("welcome#index") 
 		end
@@ -9,10 +9,18 @@ RSpec.describe WelcomeController, :type => :routing do
 end
 
 RSpec.describe SessionsController, :type => :routing do 
-	describe "routing" do 
-		it "routes to #login" do 
+	describe "routing to login page" do 
+		it "routes to sessions#new" do 
  			expect(:get => new_user_session_path).to route_to("devise/sessions#new")
 		end
 	end
+end
+
+RSpec.describe PostsController, :type => :routing do
+	describe "routing to posts index page" do 
+		it "routes to posts#index" do 
+			expect(:get => posts_path).to route_to("posts#index")
+		end
+	end 
 end
 
